b2c255e0c23ca4069e843809cae1fa6ba35dbee2
Likhith Garapati <likhithgarapati2002@gmail.com>
2026-03-18 00:07:24 +0530

    Add files via upload

---
+++ Casks/bloomer.rb
@@ -0,0 +1,12 @@
+cask "bloomer" do
+  version "1.0.0"
+  sha256 "b9332307e1f9ada1ba7984dcbeb337905ef2ac31cb738c6a82fc98c789773881  Bloomer.zip"
+
+  url "https://www.dropbox.com/scl/fi/hwkmxavvdfponfkk6bq98/Bloomer.zip?rlkey=ltvxtsuocfp1qudowmulm335r&st=vb4lw8bi&dl=1"
+  name "Bloomer"
+  desc "Desktop pet to remind you to water your plants"
+  homepage "https://github.com/likhith1542/homebrew-bloomer"
+
+  app "Bloomer.app"
+
+end
