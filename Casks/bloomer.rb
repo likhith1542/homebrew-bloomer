1d0aef3609cf1bc3a188538dabbb23b41f016650
Likhith Garapati <likhithgarapati2002@gmail.com>
2026-03-18 00:18:55 +0530

    Update bloomer.rb

--- Casks/bloomer.rb
+++ Casks/bloomer.rb
@@ -2,7 +2,7 @@ cask "bloomer" do
   version "1.0.0"
   sha256 "b9332307e1f9ada1ba7984dcbeb337905ef2ac31cb738c6a82fc98c789773881"
 
-  url "https://www.dropbox.com/scl/fi/hwkmxavvdfponfkk6bq98/Bloomer.zip?rlkey=ltvxtsuocfp1qudowmulm335r&st=vb4lw8bi&dl=1"
+  url "https://dl.dropboxusercontent.com/scl/fi/hwkmxavvdfponfkk6bq98/Bloomer.zip?rlkey=ltvxtsuocfp1qudowmulm335r"
   name "Bloomer"
   desc "Desktop pet to remind you to water your plants"
   homepage "https://github.com/likhith1542/homebrew-bloomer"
